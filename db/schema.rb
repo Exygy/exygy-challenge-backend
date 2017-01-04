a3d18cbf19c452cca1dba08581ffee88fda8d795
Pierre <pierre@exygy.com>
2017-01-04 16:06:25 +0100

    seed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104142657) do
+ActiveRecord::Schema.define(version: 20170104144144) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "name"
     t.string   "title"
     t.string   "modified_by"
-    t.date     "last_modified"
     t.string   "status"
     t.string   "country"
     t.boolean  "stared"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,90 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed Documents
+[
+  {
+    name: "List and description of investigators and sites - 234567 - 00000003548",
+    title: "16.1.4 List of Investigators",
+    modified_by: "Sophia Bishop",
+    document_type: :excel,
+    status: "final",
+    country: "Germany"
+    stared: true,
+  },
+  {
+    name: "Clinical Summary - 25399",
+    title: "Clinical Summary of Efficacy and Safety for Ibuprofen",
+    modified_by: "Lura Brown",
+    document_type: :word,
+    status: "final",
+    country: "United States"
+    stared: false,
+  },
+  {
+    name: "Approval Letter",
+    title: "NDA 94623",
+    modified_by: "Steven Aguilar",
+    document_type: :pdf,
+    status: "final",
+    country: "Germany"
+    stared: false,
+  },
+  {
+    name: "Approval Letter",
+    title: "NDA 94623",
+    modified_by: "Jimmy Townsend",
+    document_type: :word,
+    status: "final",
+    country: "Germany, United States"
+    stared: true,
+  },
+  {
+    name: "Approval Letter",
+    title: "NDA 94623",
+    modified_by: "Sophia Bishop",
+    document_type: :excel,
+    status: "final",
+    country: "Germany"
+    stared: false,
+  },
+  {
+    name: "List and description of investigators and sites - 234567 - 00000003548",
+    title: "16.1.4 List of Investigators",
+    modified_by: "Lura Brown",
+    document_type: :excel,
+    status: "final",
+    country: "United States"
+    stared: false,
+  },
+  {
+    name: "Clinical Summary - 25399",
+    title: "Clinical Summary of Efficacy and Safety for Ibuprofen",
+    modified_by: "Steven Aguilar",
+    document_type: :word,
+    status: "final",
+    country: "Germany"
+    stared: false,
+  },
+  {
+    name: "List and description of investigators and sites - 234567 - 00000003548",
+    title: "16.1.4 List of Investigators",
+    modified_by: "Jimmy Townsend",
+    document_type: :excel,
+    status: "final",
+    country: "Germany, United States"
+    stared: true,
+  },
+  {
+    name: "Clinical Summary - 25399",
+    title: "Clinical Summary of Efficacy and Safety for Ibuprofen",
+    modified_by: "Sophia Bishop",
+    document_type: :word,
+    status: "final",
+    country: "Germany"
+    stared: false,
+  }
+].each do |document|
+  Document.create(document)
+end
